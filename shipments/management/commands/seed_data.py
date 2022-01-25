f96483c6d16c9fd8e7f1bcd8a95c3ac2d118358a
Hussein-ElAttar <hussien.elattar1@gmail.com>
2022-01-25 02:54:50 +0200

    Update seeders

--- shipments/management/commands/seed_data.py
+++ shipments/management/commands/seed_data.py
@@ -31,13 +31,26 @@ class Command(BaseCommand):
             seeder.add_entity(ShipmentMethod, 1, {'name': 'Within 7 Days'})
 
         if not ShipmentStatus.objects.exists():
-            seeder.add_entity(ShipmentStatus, 10, {
-                'status': lambda x: random.choice(ShipmentStatusEnum.choices())[1]
-            })
+            seeder.add_entity(ShipmentStatus, 1, {'status':ShipmentStatusEnum.PROCCESSING })
+            seeder.add_entity(ShipmentStatus, 1, {'status':ShipmentStatusEnum.PENDING })
+            seeder.add_entity(ShipmentStatus, 1, {'status':ShipmentStatusEnum.CONFIRMED })
+            seeder.add_entity(ShipmentStatus, 1, {'status':ShipmentStatusEnum.IN_PROGRESS })
+            seeder.add_entity(ShipmentStatus, 1, {'status':ShipmentStatusEnum.DELIVERED })
+            seeder.add_entity(ShipmentStatus, 1, {'status':ShipmentStatusEnum.PENDING_CANCELATION })
+            seeder.add_entity(ShipmentStatus, 1, {'status':ShipmentStatusEnum.CANCELED })
 
         if not Shipment.objects.exists():
             seeder.add_entity(Shipment, 10, {
-                'tracking_id': lambda x: str(uuid.uuid4())
+                'tracking_id': lambda x: str(uuid.uuid4()),
+                'sender_phone_number': lambda x: seeder.faker.phone_number(),
+                'sender_address': lambda x: seeder.faker.street_address(),
+                'sender_country': lambda x: seeder.faker.country(),
+                'sender_city': lambda x: seeder.faker.city(),
+                'sender_postal_code': lambda x: seeder.faker.postcode(),
+                'receiver_address': lambda x: seeder.faker.street_address(),
+                'receiver_country': lambda x: seeder.faker.country(),
+                'receiver_city': lambda x: seeder.faker.city(),
+                'receiver_postal_code': lambda x: seeder.faker.postcode(),
+                'receiver_phone_number': lambda x: seeder.faker.phone_number(),
             })
-
         inserted_pks = seeder.execute()
